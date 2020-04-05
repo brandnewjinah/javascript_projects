7930b1359cbffbc519a86f587b7932a92a36230b
brandnewjinah <fw7128@gmail.com>
2020-04-06 06:00:59 +0900

    .

--- js_projects/bg_color_hex/script.js
+++ js_projects/bg_color_hex/script.js
@@ -1,11 +1,20 @@
 const colorBtn = document.querySelector('.colorBtn');
 const bodyBg = document.querySelector('body');
+const hexNumbers = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+const hex = document.querySelector('.hex');
 
-const colors = ['#edd2c5', '#90a1ae', '#bfafb6', '#cfdbe3', '#efe2c7v'];
+colorBtn.addEventListener('click',getHex);
 
-colorBtn.addEventListener('click', changeColor);
+function getHex() {
+    let hexCol = '#';
 
-function changeColor() {
-    let random = Math.floor(Math.random() * colors.length)
-    bodyBg.style.backgroundColor = colors[random]
+    for(let i = 0; i<6; i++) {
+        let random = Math.floor(Math.random()*hexNumbers.length);
+        hexCol += hexNumbers[random];
+    }
+
+    bodyBg.style.backgroundColor = hexCol;
+    hex.innerHTML = hexCol;
+
+
 }
